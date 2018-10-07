91bdfe0a21eaba2b2b24db68d40d676eb9532180
jaydenlaturnus <jaydenlaturnus@gmail.com>
2018-10-07 14:21:15 -0600

    Added basic PRAW functionality

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DiscordBot
+
+The purpose of this project is to become more familiar with APIs, how to use them, and becoming more
+comfortable doing so. In particular Discord's API is being used, hence the DiscordBot, because their
+application is something I use quite frequently.
+
+
--- bot.py
+++ bot.py
@@ -2,10 +2,13 @@
 import discord
 from discord.ext import commands
 import localtoken
+import praw
+import random
 
 TOKEN = localtoken.LOCALTOKEN
 
 bot = commands.Bot(command_prefix='$')
+reddit = praw.Reddit(client_id=localtoken.CLIENTID, client_secret=localtoken.CLIENTSECRET, user_agent=localtoken.USERAGENT)
 
 @bot.command()
 async def greet(ctx):
@@ -26,9 +29,19 @@ async def math(ctx, a: int, arg, b: int):
         await ctx.send(a // b)
     else:
         await ctx.send("Sorry {0.author.mention}, that command isn't something I can do!".format(ctx))
+
+@bot.command()
+async def codehumor(ctx):
+    memes = reddit.subreddit('ProgrammerHumor').hot()
+    post = random.randint(1, 50)
+
+    for x in range(0, post):
+        submission = next(x for x in memes if not x.stickied)
+
+    await ctx.send(submission.url)
     
 @bot.event
-async def on_ready():
+async def on_ready(ctx):
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
